1a9672b7c87b5660d43bf9dc9f04835d2bdfe7cf
Sergiu Strugariu <46204417+ssergiu77@users.noreply.github.com>
2021-11-24 05:35:44 -0800

    Add files via upload

---
+++ Frameworks/FiveM/[_uwu]/_uwu/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "adamant"
+games {'gta5'}
+
+client_scripts {
+    --"_uwu_login.lua",
+    "_uwu_identitate.lua",
+    "_uwu_market.lua",
+    "_uwu_hud.lua",
+    "_uwu_banca.lua",
+    "_uwu_showroom.lua"
+}
+
+server_scripts {
+    --"_uwu_logins.lua",
+    "_uwu_identitates.lua",
+    "_uwu_markets.lua",
+    "_uwu_huds.lua",
+    "_uwu_bancas.lua",
+    "_uwu_showrooms.lua"
+}
