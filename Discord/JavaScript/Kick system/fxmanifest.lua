c7fe2a328b9f1547cf2b7badd66345db5de7e42e
Sergiu Strugariu <46204417+ssergiu77@users.noreply.github.com>
2022-07-19 16:13:03 +0300

    Delete Discord directory

--- Discord/JavaScript/Kick system/fxmanifest.lua
+++
@@ -1,7 +0,0 @@
-
-fx_version 'bodacious'
-games {"gta5"}
-
-version "1.0.0"
-
-server_script "index.js"
--- Discord/Python/Stuff/decrypt.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-str = hashlib.sha256(b'test')
-
-str_hex = str.hexdigest()
-
-print(str_hex)
